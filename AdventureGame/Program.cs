cac224b868710c3b4c4d092623b9bb8b4f586d5a
Lucy Ironmonger <37961811+lucy-ironmonger@users.noreply.github.com>
2022-01-09 22:38:55 +0000

    Add project files.

---
+++ AdventureGame/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AdventureGame
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string CharacterName;
+            CharacterName = "Max Power";
+            Console.WriteLine("Adventure Game!");
+            Console.WriteLine("Welcome, thanks for coming.");
+            Console.ReadKey();
+        }
+    }
+}
